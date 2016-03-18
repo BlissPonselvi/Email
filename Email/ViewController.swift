7e8dd5b5c52412ed64dcf310524b184c1443562e
BlissPonselvi <ponselvi@blisslogix.net>
2016-03-18 14:28:57 +0530

    Email

--- Email/ViewController.swift
+++ Email/ViewController.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIAlertViewDelegate {
 
+    @IBOutlet var name: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func isValidEmail(testStr:String) -> Bool {
+        
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluateWithObject(testStr)
+        
+        
+    }
+    
+    @IBAction func Submit(sender: AnyObject) {
+        
+        if (isValidEmail(name.text!)){
+            
+            let alert : UIAlertView = UIAlertView(title: "Alert", message: "valid Login", delegate: self, cancelButtonTitle: "OK");
+            
+            alert.show()
+            
+            
+        }else{
+            
+            let alert : UIAlertView = UIAlertView(title: "Alert", message: "Invalid Login", delegate: self, cancelButtonTitle: "OK");
+            
+            alert.show()
+            
+            
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- EmailTests/EmailTests.swift
+++ EmailTests/EmailTests.swift
@@ -27,6 +27,8 @@ class EmailTests: XCTestCase {
     }
     
     func testPerformanceExample() {
+        let vc = ViewController()
+
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
